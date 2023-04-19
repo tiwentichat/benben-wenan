90bde27c7c3c727feccad2a5072d2f9a2d1aa25b
tiwentichat <125454744+tiwentichat@users.noreply.github.com>
2023-04-19 07:41:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/api/generate.ts
@@ -0,0 +1,41 @@
+import type { NextRequest } from "next/server";
+import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream";
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("Missing env var from OpenAI");
+}
+
+export const config = {
+  runtime: "edge",
+};
+
+const handler = async (req: NextRequest): Promise<Response> => {
+  const { prompt } = (await req.json()) as {
+    prompt?: string;
+  };
+
+  if (!prompt) {
+    return new Response("No prompt in the request", { status: 400 });
+  }
+
+  const payload: OpenAIStreamPayload = {
+    // model:"text-chat-davinci-002-20221122",
+    // model: "text-davinci-003",
+    // model:"text-curie-001",
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }],
+    // prompt,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 800,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+};
+
+export default handler;
